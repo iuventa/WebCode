550d7294ebc793a0f8ae48dff1caf01322c04718
iuventa <salogach35@gmail.com>
2023-03-14 01:29:27 +0400

    WebCode Start

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$('.owl-slider-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplaySpeed:1000,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+$('.owl-review-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplaySpeed:1000,
+    autoplayTimeout:5000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
